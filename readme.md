8001d6ff6dbb4c0d1cdb47a6d3734490422ecaaa
Javier Villanueva <javiervd@gmail.com>
2015-10-11 16:56:31 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Chatty
+
+A simple chat app using Laravel 5 and AngularJS
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html ng-app="chatty">
+<head>
+    <meta charset="utf-8">
+    <title>Chatty</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Chatty</h1>
+    </div>
+
+    <script src="/js/all.js"></script>
+</body>
+</html>
