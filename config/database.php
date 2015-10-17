a6b040bb580e7e3ea82536b123cde3eb73525aa8
Javier Villanueva <javiervd@gmail.com>
2015-10-17 17:14:07 +0100

    Update redis config host to env variable

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => '127.0.0.1',
+            'host'     => env('CACHE1_HOST', '127.0.0.1'),
             'port'     => 6379,
             'database' => 0,
         ],
