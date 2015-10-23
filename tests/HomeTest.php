3423e218c293c7002df7e8c7913525ad276b9a09
Javier Villanueva <javiervd@gmail.com>
2015-10-23 22:05:57 +0100

    Add tests for homepage route

---
+++ tests/HomeTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Test homepage route
+ */
+class HomeTest extends TestCase
+{
+    /**
+     * Test that homepage loads correctly
+     *
+     * @return void
+     */
+    public function testHomepageRendersCorrectly()
+    {
+        $this->call('GET', '/');
+
+        $this->assertResponseOk();
+    }
+}
